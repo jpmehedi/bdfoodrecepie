b22ee03b1a469485e515d5fbac9d812a0f38fb6a
Mehedi Hasan <mehedi@augnitive.com>
2023-08-31 00:47:06 +0900

    Webview issue fixed

--- lib/screen/video/video.dart
+++ lib/screen/video/video.dart
@@ -1,8 +1,7 @@
 
+import 'package:bdfoodrecipe/screen/video/web_view_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
-import 'web_view_controller.dart';
 
 
  class YoutubeScreen extends StatefulWidget {
@@ -13,16 +12,17 @@ import 'web_view_controller.dart';
 
  class YoutubeScreenState extends State<YoutubeScreen> {
 
+
    @override
    Widget build(BuildContext context) {
-     final Map data = ModalRoute.of(context)!.settings.arguments as Map;
+    final Map data = ModalRoute.of(context)!.settings.arguments as Map;
      return Scaffold(
        appBar: AppBar(
          backgroundColor: Colors.red,
          title: Text(data["title"]),
        ),
       body: WebViewWidget(
-        controller: WebViewControllerClass.loadWebData(data),
+        controller: WebviewClass.loadWebView(data)
       ),
      );
    }
